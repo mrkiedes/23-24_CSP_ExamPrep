37541aff400175bb7ba7c6ce0e071f5992fb4fee
mrkiedes <akiedes@vtsd.com>
2024-04-29 07:17:47 -0400

    Sorting and Searching

---
+++ binarySearch.py
@@ -0,0 +1,17 @@
+# Binary Search in python
+
+
+def binarySearch(array, x, low, high):
+
+
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
---
+++ bubbleSort.py
@@ -0,0 +1,16 @@
+# Bubble sort in Python
+
+def bubbleSort(array):
+
+
+
+
+
+    
+
+data = [-2, 45, 0, 11, -9]
+
+bubbleSort(data)
+
+print('Sorted Array in Ascending Order:')
+print(data)
